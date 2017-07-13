642c8742b402d4f76bf7d4a05e75f453eb735003
Kevin Sullivan <kevin@bitantics.com>
2017-07-12 22:31:46 -0700

    Minimized CSS

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    require( 'postcss-import' )(),
+    require( 'postcss-cssnext' )(),
+    require( 'cssnano' )({
+      preset: 'default',
+      autoprefixer: false,
+    }),
+  ]
+}
