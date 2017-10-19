bec40966fa49e0ea5e528919d201a10b03b96c20
Kevin Sullivan <kevin.sullivan@axolotl.industries>
2017-10-18 19:43:16 -0700

    Smart quotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,10 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [ { resolve: `gatsby-remark-prismjs` } ],
+        plugins: [ 
+          'gatsby-remark-prismjs',
+          'gatsby-remark-smartypants',
+        ],
       },
     },
     {
