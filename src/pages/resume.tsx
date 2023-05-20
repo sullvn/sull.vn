3e108bd346f981a1213363b158a5b9d6d907ecf2
Kevin Sullivan <kevin@sull.vn>
2023-05-19 19:46:44 -0500

    Fix resume text (#52)
    A job description had misplaced periods and missing article words.

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -25,9 +25,9 @@ export default function ResumePage(): JSX.Element {
           <Duration>2020 - 2022</Duration>
         </Job>
         <Task>
-          <Role>Co-founder.</Role> Helped create plant soil sensor.
-          product. Worked on app, cloud services, embedded software,
-          manufacturing, packaging, shop, and media.
+          <Role>Co-founder.</Role> Helped create the plant soil
+          sensor product. Worked on app, cloud services, embedded
+          software, manufacturing, packaging, shop, and media.
         </Task>
 
         <Job>
