33b8e03113de9eab68fc7dffd5e468f6507c515d
Kevin Sullivan <kevin@sull.vn>
2026-02-14 23:23:10 +0000

    Bring back colored shapes

--- astro.config.ts
+++ astro.config.ts
@@ -25,5 +25,10 @@ export default defineConfig({
     css: {
       transformer: 'lightningcss',
     },
+    server: {
+      watch: {
+        ignored: ['**/dist/**', '**/.*/**'],
+      },
+    },
   },
 })
