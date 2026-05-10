bc5cf038eeb8bf74aa2fff739922802eac9bec01
Kevin Sullivan <kevin@sull.vn>
2026-05-10 00:06:39 +0000

    Better code and Katex scrollbars

---
+++ src/config/layout.ts
@@ -0,0 +1,4 @@
+export const bodyPaddingBlockStart = 'max(2rem, env(safe-area-inset-top))'
+export const bodyPaddingBlockEnd = 'max(2rem, env(safe-area-inset-bottom))'
+export const bodyPaddingInlineStart = 'max(2rem, env(safe-area-inset-left))'
+export const bodyPaddingInlineEnd = 'max(2rem, env(safe-area-inset-right))'
